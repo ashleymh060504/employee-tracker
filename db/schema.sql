cb9f1cd0046049d87f388ebec38959f9a7c3cf9f
Ashley Hayes <ashleymh060504@gmail.com>
2024-12-02 14:39:30 -0500

    worked on schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,28 @@ create database employee_tracker_db;
 
 \c employee_tracker_db;
 
+create table departments (
+    department_id int primary key,
+    department_name varchar(30) not null
+);
+
+create table roles (
+    role_id int primary key,
+    role_title varchar(30) not null,
+    role_salary decimal(10, 2),
+    department_name varchar(30) not null,
+    foreign key (department_name) references departments(department_name)
+);
+
 create table employees (
     employee_id int primary key,
-    employee_name varchar(30),
-    employee_role varchar(30),
-    department varchar(30),
+    employee_firstName varchar(30) not null,
+    employee_lastName varchar(30) not null,
+    employee_role varchar(30) not null,
+    department_name varchar(30) not null,
+    employee_salary decimal(10, 2),
+    manager varchar(30),
+    foreign key (employee_role) references roles(role_title),
+    foreign key (department_name) references departments(department_name)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,33 @@
-insert into employees (employee_name, employee_role, department) 
-values ('Liam Carter', 'Manager', 'HR'),
-       ('Olivia Smith', 'Assistant Manager', 'HR'),
-       ('Noah Johnson', 'HR Specialist', 'HR'),
-       ('Emma Williams', 'Manager', 'Finance'),
-       ('William Brown', 'Assistant Manager', 'Finance'),
-       ('Ava Jones', 'Finance Specialist', 'Finance'),
-       ('James Garcia', 'Manager', 'IT'),
-       ('Isabella Martinez', 'Assistant Manager', 'IT'),
-       ('Logan Rodriguez', 'IT Specialist', 'IT');
+insert into departments (department_name) 
+values  ('HR'), 
+        ('Finance'), 
+        ('IT');
+
+insert into roles (role_title, role_salary, department_name)
+values ('Manager', 100000.00, 'HR'),
+       ('Assistant Manager', 80000.00, 'HR'),
+       ('HR Specialist', 60000.00, 'HR'),
+       ('Manager', 100000.00, 'Finance'),
+       ('Assistant Manager', 80000.00, 'Finance'),
+       ('Finance Specialist', 60000.00, 'Finance'),
+       ('Manager', 100000.00, 'IT'),
+       ('Assistant Manager', 80000.00, 'IT'),
+       ('IT Specialist', 60000.00, 'IT');
+
+insert into employees (
+    employee_firstName, 
+    employee_lastName, 
+    employee_role, 
+    department_name, 
+    employee_salary, 
+    manager) 
+values ('Liam', 'Carter', 'Manager', 'HR', 100000.00, NULL),
+       ('Olivia', 'Smith', 'Assistant Manager', 'HR', 80000.00, 'Liam Carter'),
+       ('Noah', 'Johnson', 'HR Specialist', 'HR', 60000.00, 'Olivia Smith'),
+       ('Emma', 'Williams', 'Manager', 'Finance', 100000.00, NULL),
+       ('William', 'Brown', 'Assistant Manager', 'Finance', 80000.00, 'Emma Williams'),
+       ('Ava', 'Jones', 'Finance Specialist', 'Finance', 60000.00, 'William Brown'),
+       ('James', 'Garcia', 'Manager', 'IT', 100000.00, NULL),
+       ('Isabella', 'Martinez', 'Assistant Manager', 'IT', 80000.00, 'James Garcia'),
+       ('Logan', 'Rodriguez', 'IT Specialist', 'IT', 60000.00, 'Isabella Martinez');
+
